f59d551fdb96480c4fb1f18357d5f734dbbbc648
Tomáš Krch <dev@krch.tech>
2022-08-15 14:31:22 +0200

    #53 c# lang ver 7.3 for easy usage of legacy handler

--- Migration.Toolkit.KXP.Extensions/LegacyAttachmentHandler.cs
+++ Migration.Toolkit.KXP.Extensions/LegacyAttachmentHandler.cs
@@ -14,82 +14,85 @@ using Migration.Toolkit.KXP.Extensions;
 [assembly: RegisterImplementation(typeof(AttachmentsService), typeof(AttachmentsService), Lifestyle = Lifestyle.Transient, Priority = RegistrationPriority.SystemDefault)]
 [assembly: RegisterModule(typeof(LegacyAttachmentHandler))]
 
-namespace Migration.Toolkit.KXP.Extensions;
-
-public class LegacyAttachmentHandler : CMS.DataEngine.Module
+namespace Migration.Toolkit.KXP.Extensions
 {
-    public LegacyAttachmentHandler()
-        : base("LegacyAttachmentHandler.Handler")
+    public class LegacyAttachmentHandler : CMS.DataEngine.Module
     {
-    }
-
-    /// <summary>
-    /// Handles the module pre-initialization.
-    /// </summary>
-    protected override void OnPreInit()
-    {
-        base.OnPreInit();
-        RegisterMediaFileHandler("getimage/{fileguid:guid}/{filename}", 4);
-        RegisterMediaFileHandler("getattachment/{fileguid:guid}/{filename}", 5);
-        RegisterMediaFileHandler("getattachment/{*pathandfilename}", 6, typeof(AttachmentsService));
-    }
-
-    private static void RegisterMediaFileHandler(string routeTemplate, int order, Type? customHandlerType = null)
-    {
-        var registerMethod = HttpHandlerRouteTable.Default.GetType().GetMethod("Register", BindingFlags.NonPublic | BindingFlags.Instance);
-        var handlerType = customHandlerType ?? typeof(MediaFileInfo).Assembly.GetType("CMS.MediaLibrary.GetMediaService");
-
-        registerMethod!.Invoke(HttpHandlerRouteTable.Default, new object[]
+        public LegacyAttachmentHandler()
+            : base("LegacyAttachmentHandler.Handler")
         {
-            new RegisterHttpHandlerAttribute(routeTemplate, handlerType)
-            {
-                Order = order
-            }
-        });
-    }
-}
-
-public class AttachmentsService : ActionResultServiceBase
-{
-    private readonly ISiteService siteService;
-
-    public AttachmentsService(ISiteService siteService)
-    {
-        this.siteService = siteService;
-    }
-
-    protected override RequestStatusEnum RequestStatusEnum => RequestStatusEnum.GetFileHandler;
-
-    protected override CMSActionResult GetActionResultInternal()
-    {
-        var pathAndFileName = QueryHelper.GetString("pathandfilename", null);
-        if (pathAndFileName != null)
-        {
-            pathAndFileName = pathAndFileName.TrimEnd('/');
-            var dir = Path.GetDirectoryName(pathAndFileName);
-            var mediaFiles = MediaFileInfoProvider.ProviderObject.Get()
-                .WhereStartsWith(nameof(MediaFileInfo.FilePath), dir)
-                .ToArray();
-
-            var mf = mediaFiles
-                .FirstOrDefault(mf =>
-                    (string)mf.FileCustomData.GetValue("__LegacyOriginalPath") == $"/{dir}" &&
-                    mf.FilePath.EqualsCSafe(pathAndFileName)
-                );
-            
-            if (mf != null && siteService.CurrentSite is { } site && pathAndFileName.LastIndexOf('/') is var fileNameIndex and >= 0)
-            {
-                var fileName = pathAndFileName[(fileNameIndex + 1)..];
-                var mediaPath = MediaFileInfoProvider.GetMediaFilePath(mf, site.SiteName, SystemContext.WebApplicationPhysicalPath);
-                var result = new CMSPhysicalFileResult(mediaPath)
-                {
-                    ContentType = mf.FileMimeType,
-                    ContentDisposition = HTTPHelper.GetFileDisposition(fileName, Path.GetExtension(mediaPath))
-                };
-                return result;
-            }
         }
 
-        return new CMSNotFoundResult();
+        /// <summary>
+        /// Handles the module pre-initialization.
+        /// </summary>
+        protected override void OnPreInit()
+        {
+            base.OnPreInit();
+            RegisterMediaFileHandler("getimage/{fileguid:guid}/{filename}", 4);
+            RegisterMediaFileHandler("getattachment/{fileguid:guid}/{filename}", 5);
+            RegisterMediaFileHandler("getattachment/{*pathandfilename}", 6, typeof(AttachmentsService));
+        }
+
+        private static void RegisterMediaFileHandler(string routeTemplate, int order, Type customHandlerType = null)
+        {
+            var registerMethod = HttpHandlerRouteTable.Default.GetType().GetMethod("Register", BindingFlags.NonPublic | BindingFlags.Instance);
+            var handlerType = customHandlerType ?? typeof(MediaFileInfo).Assembly.GetType("CMS.MediaLibrary.GetMediaService");
+
+            registerMethod.Invoke(HttpHandlerRouteTable.Default, new object[]
+            {
+                new RegisterHttpHandlerAttribute(routeTemplate, handlerType)
+                {
+                    Order = order
+                }
+            });
+        }
+    }
+
+    public class AttachmentsService : ActionResultServiceBase
+    {
+        private const string LegacyOriginalPath = "__LegacyOriginalPath";
+        private readonly ISiteService siteService;
+
+        public AttachmentsService(ISiteService siteService)
+        {
+            this.siteService = siteService;
+        }
+
+        protected override RequestStatusEnum RequestStatusEnum => RequestStatusEnum.GetFileHandler;
+
+        protected override CMSActionResult GetActionResultInternal()
+        {
+            var pathAndFileName = QueryHelper.GetString("pathandfilename", null);
+            if (pathAndFileName != null)
+            {
+                pathAndFileName = pathAndFileName.TrimEnd('/');
+                var dir = Path.GetDirectoryName(pathAndFileName);
+                var mediaFiles = MediaFileInfoProvider.ProviderObject.Get()
+                    .WhereStartsWith(nameof(MediaFileInfo.FilePath), dir)
+                    .ToArray();
+
+                var mediaFile = mediaFiles
+                    .FirstOrDefault(mf =>
+                        (string)mf.FileCustomData.GetValue(LegacyOriginalPath) == $"/{dir}" &&
+                        mf.FilePath.EqualsCSafe(pathAndFileName)
+                    );
+
+                if (mediaFile != null && siteService.CurrentSite is ISiteInfo site && pathAndFileName.LastIndexOf('/') is var fileNameIndex &&
+                    fileNameIndex >= 0)
+                {
+                    var fileName = pathAndFileName.Substring((fileNameIndex + 1), 1);
+                    var mediaPath = MediaFileInfoProvider.GetMediaFilePath(mediaFile, site.SiteName, SystemContext.WebApplicationPhysicalPath);
+                    var result = new CMSPhysicalFileResult(mediaPath)
+                    {
+                        ContentType = mediaFile.FileMimeType,
+                        ContentDisposition = HTTPHelper.GetFileDisposition(fileName, Path.GetExtension(mediaPath))
+                    };
+                    return result;
+                }
+            }
+
+            return new CMSNotFoundResult();
+        }
     }
 }
