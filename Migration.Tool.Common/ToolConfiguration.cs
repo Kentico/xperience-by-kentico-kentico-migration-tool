08073b2cf2db7624cb8bdc0b50dcfd177b94cec0
Anthony Marquez <AnthonyM@kentico.com>
2024-10-24 10:30:30 -0400

    Added content item conversion

--- Migration.Tool.Common/ToolConfiguration.cs
+++ Migration.Tool.Common/ToolConfiguration.cs
@@ -49,9 +49,17 @@ public class ToolConfiguration
     [ConfigurationKeyName(ConfigurationNames.CreateReusableFieldSchemaForClasses)]
     public string? CreateReusableFieldSchemaForClasses { get; set; }
 
+    [ConfigurationKeyName(ConfigurationNames.ConvertClassesToContentHub)]
+    public string? ConvertClassesToContentHub { get; set; }
+
 
     public IReadOnlySet<string> ClassNamesCreateReusableSchema => classNamesCreateReusableSchema ??= new HashSet<string>(
         (CreateReusableFieldSchemaForClasses?.Split(new[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries) ?? []).Select(x => x.Trim()),
+        StringComparer.InvariantCultureIgnoreCase
+    );
+
+    public IReadOnlySet<string> ClassNamesConvertToContentHub => classNamesConvertToContentHub ??= new HashSet<string>(
+        (ConvertClassesToContentHub?.Split(new[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries) ?? []).Select(x => x.Trim()),
         StringComparer.InvariantCultureIgnoreCase
     );
 
@@ -97,6 +105,7 @@ public class ToolConfiguration
     #region Path to root directory of target instance
 
     private HashSet<string>? classNamesCreateReusableSchema;
+    private HashSet<string>? classNamesConvertToContentHub;
     private string? xbKConnectionString;
 
     [ConfigurationKeyName(ConfigurationNames.XbKDirPath)]
