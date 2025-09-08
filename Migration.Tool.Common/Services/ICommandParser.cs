3967adc58e806935e8da90b9ccd3a5802887c685
akfakmot <akfakmot@gmail.com>
2025-09-08 14:34:51 +0200

    Add database patching mechanism & patch of content item related GUIDs
    Add: Database patching mechanism
    Add: Patch of content item related GUIDs

--- Migration.Tool.Common/Services/ICommandParser.cs
+++ Migration.Tool.Common/Services/ICommandParser.cs
@@ -4,5 +4,5 @@ namespace Migration.Tool.Common.Services;
 
 public interface ICommandParser
 {
-    List<ICommand> Parse(Queue<string> args, ref bool bypassDependencyCheck, bool firstHaveToBeMigrate = true);
+    (MasterCommand? masterCommand, List<ICommand> commands) Parse(Queue<string> args, ref bool bypassDependencyCheck);
 }
---
+++ Migration.Tool.Common/Services/MasterCommand.cs
@@ -0,0 +1,3 @@
+namespace Migration.Tool.Common.Services;
+
+public enum MasterCommand { Migrate, Patch }
