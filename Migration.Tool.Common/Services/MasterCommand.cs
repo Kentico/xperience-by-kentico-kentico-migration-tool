58c1edcced498695f84cf27afa6668181fb7b4b1
Tomáš Kafka <kafka@nittin.cz>
2025-09-19 15:56:44 +0200

    Add documentation
    Doc: Add documentation

--- Migration.Tool.Common/Services/MasterCommand.cs
+++ Migration.Tool.Common/Services/MasterCommand.cs
@@ -1,3 +1,17 @@
 namespace Migration.Tool.Common.Services;
 
-public enum MasterCommand { Migrate, Patch }
+/// <summary>
+/// Master command passed to CLI tool
+/// </summary>
+public enum MasterCommand
+{
+    /// <summary>
+    /// Migrate data. Database patching will be performed before the migration
+    /// </summary>
+    Migrate,
+
+    /// <summary>
+    /// Only patch the database, don't mgirate any data
+    /// </summary>
+    Patch
+}
